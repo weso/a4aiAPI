7500e9a01df09710e8e7e30e8d176741183cb26f
Herminio García <herminiogg@hotmail.com>
2015-01-26 14:55:11 +0100

    Closes #3

--- api/api.py
+++ api/api.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../../wixDom'))
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../../a4aiDom'))
 from infrastructure.mongo_repos.area_repository import AreaRepository
 from infrastructure.mongo_repos.indicator_repository import IndicatorRepository
 from infrastructure.mongo_repos.observation_repository import ObservationRepository
@@ -19,20 +19,36 @@ from infrastructure.mongo_repos.ranking_repository import RankingRepository
 # bson
 from bson.json_util import dumps
 
+
 ##########################################################################################
 ##                                 JSONP DECORATOR                                      ##
 ##########################################################################################
 
-def JSONEncoder(request, data):
+def json_response(request, data):
+    data = success(data)
     json = dumps(data)
-
     callback = request.args.get('callback', False)
-
     if callback:
-        return Response(str(callback) + '(' + str(json) + ');', mimetype = "application/javascript")
+        return Response(str(callback) + '(' + str(json) + ');', mimetype="application/javascript")
+    return Response(json, mimetype="application/json")
 
-    return Response(json, mimetype = "application/json")
 
+def json_encoder(request, data):
+    # Check first in order to convert to dict
+    if isinstance(data, list):
+        data = [data_element.to_dict() for data_element in data]
+    else:
+        data = data.to_dict()
+
+    return json_response(request, data)
+
+
+##########################################################################################
+##                                SUCCESS FUNCTION                                      ##
+##########################################################################################
+
+def success(data):
+    return {"success": True, "data": data}
 
 ##########################################################################################
 ##                                        ROOT                                          ##
@@ -55,34 +71,34 @@ def list_areas():
 
     areas = AreaRepository(url_root=request.url_root).find_areas(order)
 
-    return JSONEncoder(request, areas)
+    return json_encoder(request, areas)
 
 
 @app.route("/areas/countries")
 def list_countries():
     order = request.args.get('orderBy')
     countries = AreaRepository(url_root=request.url_root).find_countries(order)
-    return JSONEncoder(request, countries)
+    return json_encoder(request, countries)
 
 
 @app.route("/areas/continents")
 def list_continents():
     order = request.args.get('orderBy')
     continents = AreaRepository(url_root=request.url_root).find_continents(order)
-    return JSONEncoder(request, continents)
+    return json_encoder(request, continents)
 
 
 @app.route("/areas/<area_code>")
 def show_area(area_code):
     area = AreaRepository(url_root=request.url_root).find_countries_by_code_or_income(area_code)
-    return JSONEncoder(request, area)
+    return json_encoder(request, area)
 
 
 @app.route("/areas/<area_code>/countries")
 def show_area_countries(area_code):
     order = request.args.get('orderBy')
-    countries = AreaRepository(url_root=request.url_root).find_countries_by_continent_or_income(area_code, order)
-    return JSONEncoder(request, countries)
+    countries = AreaRepository(url_root=request.url_root).find_countries_by_continent_or_income_or_type(area_code, order)
+    return json_encoder(request, countries)
 
 
 ##########################################################################################
@@ -93,87 +109,87 @@ def show_area_countries(area_code):
 def list_indicators():
     indicators = IndicatorRepository(url_root=request.url_root).find_indicators()
 
-    return JSONEncoder(request, indicators)
+    return json_encoder(request, indicators)
 
 
 @app.route("/indicators/index")
 def show_index():
     _index = IndicatorRepository(url_root=request.url_root).find_indicators_index()
-    return JSONEncoder(request, _index)
+    return json_encoder(request, _index)
 
 
 @app.route("/indicators/subindices")
 def list_subindices():
     subindices = IndicatorRepository(url_root=request.url_root).find_indicators_sub_indexes()
-    return JSONEncoder(request, subindices)
+    return json_encoder(request, subindices)
 
 
 @app.route("/indicators/components")
 def list_components():
     components = IndicatorRepository(url_root=request.url_root).find_indicators_components()
-    return JSONEncoder(request, components)
+    return json_encoder(request, components)
 
 
 @app.route("/indicators/primary")
 def list_primary():
     primary = IndicatorRepository(url_root=request.url_root).find_indicators_primary()
-    return JSONEncoder(request, primary)
+    return json_encoder(request, primary)
 
 
 @app.route("/indicators/secondary")
 def list_secondary():
     secondary = IndicatorRepository(url_root=request.url_root).find_indicators_secondary()
-    return JSONEncoder(request, secondary)
+    return json_encoder(request, secondary)
 
 
 @app.route("/indicators/<indicator_code>")
 def show_indicator(indicator_code):
     indicator = IndicatorRepository(url_root=request.url_root).find_indicators_by_code(indicator_code)
-    return JSONEncoder(request, indicator)
+    return json_encoder(request, indicator)
 
 
 @app.route("/indicators/<indicator_code>/components")
 def list_indicator_components(indicator_code):
     indicator = IndicatorRepository(url_root=request.url_root).find_indicators_by_code(indicator_code)
 
-    if indicator["success"] is False:
-        return JSONEncoder(request, indicator)
+    if indicator is None:
+        return json_encoder(request, indicator)
 
-    components = IndicatorRepository(url_root=request.url_root).find_indicators_components(indicator["data"])
-    return JSONEncoder(request, components)
+    components = IndicatorRepository(url_root=request.url_root).find_indicators_components(indicator)
+    return json_encoder(request, components)
 
 
 @app.route("/indicators/<indicator_code>/indicators")
 def list_indicator_indicators(indicator_code):
     indicator = IndicatorRepository(url_root=request.url_root).find_indicators_by_code(indicator_code)
 
-    if indicator["success"] is False:
-        return JSONEncoder(request, indicator)
+    if indicator is None:
+        return json_encoder(request, indicator)
 
-    indicators = IndicatorRepository(url_root=request.url_root).find_indicators_indicators(indicator["data"])
-    return JSONEncoder(request, indicators)
+    indicators = IndicatorRepository(url_root=request.url_root).find_indicators_indicators(indicator)
+    return json_encoder(request, indicators)
 
 
 @app.route("/indicators/<indicator_code>/primary")
 def list_indicator_primary(indicator_code):
     indicator = IndicatorRepository(url_root=request.url_root).find_indicators_by_code(indicator_code)
 
-    if indicator["success"] is False:
-        return JSONEncoder(request, indicator)
+    if indicator is None:
+        return json_encoder(request, indicator)
 
-    primary = IndicatorRepository(url_root=request.url_root).find_indicators_primary(indicator["data"])
-    return JSONEncoder(request, primary)
+    primary = IndicatorRepository(url_root=request.url_root).find_indicators_primary(indicator)
+    return json_encoder(request, primary)
 
 
 @app.route("/indicators/<indicator_code>/secondary")
 def list_indicator_secondary(indicator_code):
     indicator = IndicatorRepository(url_root=request.url_root).find_indicators_by_code(indicator_code)
 
-    if indicator["success"] is False:
-        return JSONEncoder(request, indicator)
+    if indicator is None:
+        return json_encoder(request, indicator)
 
-    secondary = IndicatorRepository(url_root=request.url_root).find_indicators_secondary(indicator["data"])
-    return JSONEncoder(request, secondary)
+    secondary = IndicatorRepository(url_root=request.url_root).find_indicators_secondary(indicator)
+    return json_encoder(request, secondary)
 
 
 ##########################################################################################
@@ -182,28 +198,28 @@ def list_indicator_secondary(indicator_code):
 @app.route("/observations")
 def list_observations():
     observations = ObservationRepository(url_root=request.url_root).find_observations()
-    return JSONEncoder(request, observations)
+    return json_encoder(request, observations)
 
 @app.route("/linked-observations")
 def list_linked_observations():
     linked_obs = ObservationRepository(url_root=request.url_root).find_linked_observations()
-    return JSONEncoder(request, linked_obs)
+    return json_encoder(request, linked_obs)
 
 @app.route("/observations/<indicator_code>")
 def list_observations_by_indicator(indicator_code):
     observations = ObservationRepository(url_root=request.url_root).find_observations(indicator_code)
-    return JSONEncoder(request, observations)
+    return json_encoder(request, observations)
 
 @app.route("/observations/<indicator_code>/<area_code>")
 def list_observations_by_indicator_and_country(indicator_code, area_code):
     observations = ObservationRepository(url_root=request.url_root).find_observations(indicator_code, area_code)
-    return JSONEncoder(request, observations)
+    return json_encoder(request, observations)
 
 
 @app.route("/observations/<indicator_code>/<area_code>/<year>")
 def list_observations_by_indicator_and_country_and_year(indicator_code, area_code, year):
     observations = ObservationRepository(url_root=request.url_root).find_observations(indicator_code, area_code, year)
-    return JSONEncoder(request, observations)
+    return json_encoder(request, observations)
 
 
 ##########################################################################################
@@ -213,12 +229,13 @@ def list_observations_by_indicator_and_country_and_year(indicator_code, area_cod
 @app.route("/years")
 def list_observations_years():
     years = ObservationRepository(url_root=request.url_root).get_year_list()
-    return JSONEncoder(request, years)
+    return json_encoder(request, years)
 
 @app.route("/years/array")
 def list_observations_years_array():
-    years = ObservationRepository(url_root=request.url_root).get_year_array()
-    return JSONEncoder(request, years)
+    years = ObservationRepository(url_root=request.url_root).get_year_list()
+    years_array = [year.value for year in years]
+    return json_response(request, years_array)
 
 ##########################################################################################
 ##                                    VISUALISATIONS                                    ##
@@ -227,7 +244,7 @@ def list_observations_years_array():
 @app.route("/visualisations/<indicator_code>/<area_code>/<year>")
 def list_visualisations(indicator_code, area_code, year):
     visualisations = ObservationRepository(url_root=request.url_root).find_visualisations(indicator_code, area_code, year)
-    return JSONEncoder(request, visualisations)
+    return json_encoder(request, visualisations)
 
 ##########################################################################################
 ##                                       RANKINGS                                       ##
@@ -238,12 +255,12 @@ def list_last_rankings():
     years = ObservationRepository(url_root=request.url_root).get_year_list()
     year = years["data"][0]["value"]
     rankings = RankingRepository(url_root=request.url_root).find_rankings(year)
-    return JSONEncoder(request, rankings)
+    return json_encoder(request, rankings)
 
 @app.route("/rankings/<year>")
 def list_rankings(year):
     rankings = RankingRepository(url_root=request.url_root).find_rankings(year)
-    return JSONEncoder(request, rankings)
+    return json_encoder(request, rankings)
 
 ##########################################################################################
 ##                                         HOME                                        ##
@@ -281,7 +298,7 @@ def list_home(indicators, limits, tendencies, values):
             result["observations%s" % index] = observations
             result["percentage%s" % index] = percentage
 
-    return JSONEncoder(request, result)
+    return json_encoder(request, result)
 
 def get_percentage(observations, limit, tendency):
     count = 0
